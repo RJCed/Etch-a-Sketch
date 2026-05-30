6132e93461f496bc37d882886fb7885b7c7845e3
arjay <256202735+RJCed@users.noreply.github.com>
2026-05-30 11:16:35 +0800

    add horizontal containers & function to grid btn

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello")
+
+// Create the horizontal rows (containers)
+const createRows = function(numRows){
+    const container = document.getElementById("container");
+
+    for (i = 0; i < numRows; i++){
+        const horizontalGrid = document.createElement("div");
+
+        horizontalGrid.classList.add("horizontalGrid")
+        container.appendChild(horizontalGrid);
+    }
+}
+
+
+const btnGrid = document.getElementById("btn-grid");
+btnGrid.addEventListener("click", () => {
+    const gridNumber = prompt("Grid size: (2 - 100)");
+    createRows(gridNumber);
+})
