643ee361946c81bfc2a7ffdc981c319f53764469
arjay <256202735+RJCed@users.noreply.github.com>
2026-05-30 16:10:12 +0800

    add working rows and columns changes: -change vertical grid to createGrid to make it a general function -createGrid function creates Grid based on user input

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 // Create the vertical Columns (containers)
-const createColumns = function(numColumns){
+const createGrid = function(gridSize){
     const container = document.getElementById("container");
 
-    for (i = 0; i < numColumns; i++){
+    for (i = 0; i < gridSize; i++){
+        //This creates the columns/row containers
         const verticalGrid = document.createElement("div");
+
+
+        //This creates each box in row container
+        for (x = 0; x < gridSize; x++){
+            const horizontalGrid = document.createElement("div");
+            horizontalGrid.classList.add("horizontalGrid");
+            verticalGrid.appendChild(horizontalGrid);
+        }
+
 
         verticalGrid.classList.add("verticalGrid")
         container.appendChild(verticalGrid);
@@ -11,7 +21,17 @@ const createColumns = function(numColumns){
 }
 
 
-//remove all grid divs
+// Create the horizontal rows 
+const createRows = function(numRows){
+    const columnContainer = document.getElementsByClassName("verticalGrid")
+    // It might be wise to integrate this into the for loop of createColums
+}
+
+
+
+
+
+//remove all grid divs (Temporary Function, may change to resetting colors)
 const resetGrid = function(){
     const container = document.getElementById("container");
     container.replaceChildren();
@@ -23,15 +43,15 @@ const btnGrid = document.getElementById("btn-grid");
 btnGrid.addEventListener("click", () => {
     resetGrid();
     const gridNumber = prompt("Grid size: (2 - 100)");
-    createColumns(gridNumber);
+    createGrid(gridNumber);
 })
 
 
-// Button for removing Grid
+// Button for removing Grid (Temporary Function, may change to resetting colors)
 const btnReset = document.getElementById("btn-reset");
 btnReset.addEventListener("click", resetGrid);
 
 
 
 //temporary for game start
-createColumns(5);
+createGrid(10);
